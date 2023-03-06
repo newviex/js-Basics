8c8b370229601807357091f03a91afe7131beba7
Aldrien Allanigue <xaldrienallanigue@gmal.com>
2023-03-06 20:09:22 +0800

    display the given varibles

--- js/declaringVar.js
+++ js/declaringVar.js
@@ -7,8 +7,24 @@ myNum = myVar ; // Initialize the value of myVar to myNum
 // console.log(myNum);
 
 var a ;
-a = 7;
-var b;
-b = a
+// a = 7;
+// var b;
+// b = a
 
+// console.log(b);
+
+var a = 5;
+var b = 10;
+var c = "I am Aldrien";
+
+a = a + 1;
+b = b + 5;
+c = c + " string!";
+
+console.log(a);
 console.log(b);
+console.log(c);
+
+// console.log(a + 1);
+// console.log(b + 5);
+// console.log(c + ' string');
