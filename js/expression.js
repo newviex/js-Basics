d73d40668b5dca92463b0f233462c8b7a5d65ae3
Aldrien Allanigue <xaldrienallanigue@gmal.com>
2023-03-06 21:58:13 +0800

    add expressio coding challenge

---
+++ js/expression.js
@@ -0,0 +1,19 @@
+// Types
+
+// 1. Type of Variables
+// let a;
+
+// 2. Assigning a value of variable initialization
+// a = 4;
+
+// 3. Performing an evalutaion that will be returning a single value
+
+let b = 3;
+let c = 2; 
+let a = b + c ;
+
+// 1.
+// 2.
+// 3.
+
+console.log(a);
