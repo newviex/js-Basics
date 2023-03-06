24be7eeb1d52b9a7f8b30044b18614bc7566eb81
Aldrien Allanigue <xaldrienallanigue@gmal.com>
2023-03-06 21:13:47 +0800

    add the data types example

---
+++ js/dataType.js
@@ -0,0 +1,15 @@
+// let a = 7;
+let a; //undefined  
+let b = 8; //number
+let c = true;//boolean
+let d = 'Hello World!'; //string
+let e;
+
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+console.log(typeof d);
+console.log(typeof e);
+console.log(e);
+console.log('10');
