06a01f8868f95c37007696a3c0b49e6f997adcca
Aldrien Allanigue <xaldrienallanigue@gmal.com>
2023-03-06 22:21:29 +0800

    add comments to operators.js

---
+++ js/operators.js
@@ -0,0 +1,41 @@
+ // CATEGORIES OF OPERATORS
+
+  // 1. Assignment  '='
+  // 2. Arithmetic Operators '+ - * /'
+  // 3. Incremet and Decrement '++ --'
+
+  var a = 1;
+  a++;
+  console.log(a);
+//   console.log(a++);
+  console.log(a++);
+
+  // arithmetic % modulues
+
+  var m = 10 % 3;
+  console.log(m);
+
+  //STRING OPERATORS
+
+  // '' +
+
+  // precedence
+
+var b = 1 + 2 * 3;
+console.log(b);
+
+// use of parenthesis to control how things are evaluated
+var b = (1 + 2) * 3;
+console.log(b);
+
+// Function invocation operators
+// Logicall && ||
+// Member accessor operator
+//Console. Allows the access to the console object properties
+// Code Block
+
+// Array Elemet access operator[]
+
+// 1. Learn that they exist.
+// 2. What their function is
+// 3. Use them as we write our program
