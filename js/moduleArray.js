b76ae742016bb2a83bbef4106e4ea56fe16930f1
newviex <xaldrienallanigue@gmail.com>
2023-03-07 20:31:39 +0800

    add module array examples

---
+++ js/moduleArray.js
@@ -0,0 +1,41 @@
+let a = [4, 8, 16, 23, 54]; // we can use numeric values
+
+let b = ['rod', 'josep', 'jash','amy']; // we can use data types
+
+
+
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+console.log(a[4]);
+console.log(a[5]);
+console.log(a)
+
+a[5] = 30;
+console.log(a);
+
+console.log(typeof a);
+
+let c = [40 ,'aldrien' , true];
+console.log(c);
+
+let d = ['aldrien','aldrien1','aldrien2','aldrien3'];
+console.log(d[3]);
+
+console.log(d.length); // normal number 1,2,3,4 not 0,1,2,3
+
+let e = [10,20,30,40,50,24];
+
+// e[10] = 54 ;
+// console.log(e);
+// console.log(e.length);
+
+e.pop(); // removing data
+e.pop();
+e.pop();
+e.pop();
+
+// e.push(54); // add an element at  the end of the array
+console.log(e);
+console.log(e.length); // bibilangin nia
