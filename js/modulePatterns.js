a0ae7466e19c8bcf18a40ba8a02b5541c492e415
newviex <xaldrienallanigue@gmail.com>
2023-03-14 15:33:22 +0800

    add design pattern example

--- js/modulePatterns.js
+++ js/modulePatterns.js
@@ -9,6 +9,8 @@ var counter = (function(){
      // return an object with full of properties
      return{
       //   value: count,
+         get: function(){return count;},
+         set: function(value){ count = value;},
         increment: function(){
             count = count += 1;
             print('After Increment');
@@ -22,13 +24,26 @@ var counter = (function(){
 
 })(); //IIEF Immediately Invoke Function Expression
 
-console.log(counter.count);
+// console.log(counter.count);
 // console.log(counter.value);
-console.log(counter.increment());
-console.log(counter.increment());
-console.log(counter.increment());
-console.log(counter.increment());
+// console.log(counter.increment());
+// console.log(counter.increment());
+// console.log(counter.increment());
+// console.log(counter.increment());
+// console.log(counter.reset()); // it reset to zero
+// console.log(counter.increment());
 // console.log(counter.value); // we accidentally created a closure lets not do this
+
+counter.increment();
+counter.increment();
+counter.increment();
+counter.increment();
+
+counter.set(7);
+console.log(counter.get());
+counter.reset();
+
+
 // GLOBAL NAMESPACE why we dont want to define a variable in the Global namespace.
 
 // 1. It is not remove from the computer memory until we navigate to a new tab. 2 . Web Browser.
